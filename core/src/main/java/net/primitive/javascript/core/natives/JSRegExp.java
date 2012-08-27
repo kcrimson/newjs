8558d3a5c5ba86a37e56fd44e77cb5ba6ec44fb9
kcrimson <none@none>
2012-08-27 11:28:50 +0200

    added class for RegExp literal implementation

---
+++ core/src/main/java/net/primitive/javascript/core/natives/JSRegExp.java
@@ -0,0 +1,49 @@
+package net.primitive.javascript.core.natives;
+
+import java.util.List;
+
+import net.primitive.javascript.core.Constructor;
+import net.primitive.javascript.core.Function;
+import net.primitive.javascript.core.Scope;
+import net.primitive.javascript.core.Scriptable;
+import net.primitive.javascript.core.ScriptableObject;
+
+public class JSRegExp extends ScriptableObject implements Function, Constructor {
+
+	@Override
+	public Object call(Scope scope, Scriptable thisObj, Object[] args) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object[] bindParameters(Object[] actualParameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Scope getScope() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Scriptable construct(Scope scope, Object[] args) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object hasInstance(Object lvar) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getParameterList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
